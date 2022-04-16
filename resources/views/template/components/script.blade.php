fe2f8527cda4ae849d8b316f82568374af3a9fad
NickyErlanggaZ <nickyerlanggaz@gmail.com>
2022-04-16 11:10:23 +0700

    add: first commit

---
+++ resources/views/template/components/script.blade.php
@@ -0,0 +1,16 @@
+<!-- Bootstrap core JavaScript-->
+<script src="{{asset("package/jquery/jquery.min.js")}}"></script>
+<script src="{{asset("package/bootstrap/js/bootstrap.bundle.min.js")}}"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="{{asset("package/jquery-easing/jquery.easing.min.js")}}"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="{{asset("js/sb-admin-2.min.js")}}"></script>
+
+<!-- Page level plugins -->
+<script src="{{asset("package/chart.js/Chart.min.js")}}"></script>
+
+<!-- Page level custom scripts -->
+<script src="{{asset("js/demo/chart-area-demo.js")}}"></script>
+<script src="{{asset("js/demo/chart-pie-demo.js")}}"></script>
