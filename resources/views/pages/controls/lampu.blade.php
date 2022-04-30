ea69674c5e65d6c087e2dc6e4b15d3c044a6ec17
NickyErlanggaZ <nickyerlanggaz@gmail.com>
2022-04-30 15:17:50 +0700

    add: control lampu page

---
+++ resources/views/pages/controls/lampu.blade.php
@@ -0,0 +1,20 @@
+@extends('template.admin')
+
+@section('title', 'Kontrol Lampu')
+
+@push('custom-style')
+    <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
+@endpush
+
+@section('content')
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Status Lampu</h6>
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <h2>test</h2>
+            </div>
+        </div>
+    </div>
+@endsection
