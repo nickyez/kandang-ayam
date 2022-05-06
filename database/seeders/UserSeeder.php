a844fdad856a861fadc78f6134b9bac19b71e978
NickyErlanggaZ <nickyerlanggaz@gmail.com>
2022-05-06 13:25:10 +0700

    add: User Seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'username'=> 'admin',
+            'email' => 'admin'.'@gmail.com',
+            'password' => Hash::make('admin'),
+            'level'=> "admin",
+            'photos_url'=> "undraw_profile.svg"
+        ]);
+    }
+}
