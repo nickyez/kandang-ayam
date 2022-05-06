e5a4c26d449571fecba79db4dc9f934b9d979414
NickyErlanggaZ <nickyerlanggaz@gmail.com>
2022-05-06 13:24:37 +0700

    add: view for CRUD users

---
+++ resources/views/pages/users/create.blade.php
@@ -0,0 +1,66 @@
+@extends('template.admin')
+
+@section('title', 'Users')
+
+@push('custom-style')
+    <link href="{{ asset('package/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+@endpush
+
+@section('content')
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <div class="d-sm-flex align-items-center justify-content-between">
+                <h6 class="m-0 font-weight-bold text-primary">Tambah Pengguna</h6>
+                <a href="{{url()->previous()}}" class="btn btn-primary btn-icon-split btn-sm">
+                    <span class="icon text-white-50">
+                        <i class="fas fa-arrow-left"></i>
+                    </span>
+                    <span class="text">Kembali</span>
+                </a>
+            </div>
+        </div>
+        <form action="#" method="POST">
+            <div class="card-body">
+                <div class="form-group">
+                    <div class="small mb-1">Profile Picture</div>
+                    <div class="custom-file">
+                        <input type="file" class="custom-file-input" accept="image/png, image/gif, image/jpeg" name="foto" id="customFile">
+                        <label class="custom-file-label" for="customFile">Choose file</label>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="small mb-1">Nama</div>
+                    <input type="text" class="form-control" id="name" placeholder="John Doe">
+                </div>
+                <div class="form-group">
+                    <div class="small mb-1">Username</div>
+                    <input type="text" class="form-control" id="username"
+                        placeholder="username maximum 8 character with alphanumeric">
+                </div>
+                <div class="form-group">
+                    <div class="small mb-1">Email</div>
+                    <input type="email" class="form-control" id="email" placeholder="name@example.com">
+                </div>
+                <div class="form-group">
+                    <div class="small mb-1">Password</div>
+                    <input type="email" class="form-control" id="email" placeholder="password minimal 8 character">
+                </div>
+                <div class="card-footer">
+                    <div class="col-sm-12 mb-3">
+                        <button type="submit" class="btn btn-primary float-right btn-sm"><i class="fas fa-plus"></i>
+                            Tambah</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+@endsection
+
+@push('page-script')
+    <script src="{{ asset('package/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('package/datatables/dataTables.bootstrap4.min.js') }}"></script>
+@endpush
+
+@push('data-script')
+    <script src="{{ asset('js/demo/datatables-demo.js') }}"></script>
+@endpush
--- resources/views/pages/users/index.blade.php
+++ resources/views/pages/users/index.blade.php
@@ -3,13 +3,21 @@
 @section('title', 'Users')
 
 @push('custom-style')
-    <link href="{{asset('package/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
+    <link href="{{ asset('package/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
 @endpush
 
 @section('content')
     <div class="card shadow mb-4">
         <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Data pengguna</h6>
+            <div class="d-sm-flex align-items-center justify-content-between">
+                <h6 class="m-0 font-weight-bold text-primary">Data pengguna</h6>
+                <a href="{{Request::url()."/create"}}" class="btn btn-primary btn-icon-split btn-sm">
+                    <span class="icon text-white-50">
+                        <i class="fas fa-plus"></i>
+                    </span>
+                    <span class="text">Tambah User</span>
+                </a>
+            </div>
         </div>
         <div class="card-body">
             <div class="table-responsive">
@@ -19,6 +27,7 @@
                             <th>Nama</th>
                             <th>Email</th>
                             <th>Foto</th>
+                            <th>action</th>
                         </tr>
                     </thead>
                     <tfoot>
@@ -26,6 +35,7 @@
                             <th>Nama</th>
                             <th>Email</th>
                             <th>Foto</th>
+                            <th>action</th>
                         </tr>
                     </tfoot>
                     <tbody>
@@ -33,6 +43,16 @@
                             <td>Nicky Erlangga</td>
                             <td>nicky@gmail.com</td>
                             <td>Edinburgh</td>
+                            <td>
+                                <a href="#" class="btn btn-sm btn-success">
+                                    <i class="fa fa-edit"></i>
+                                    <span class="text">Edit</span>
+                                </a>
+                                <a href="#" class="btn btn-sm btn-danger">
+                                    <i class="fa fa-trash"></i>
+                                    <span class="text">Delete</span>
+                                </a>
+                            </td>
                         </tr>
                     </tbody>
                 </table>
