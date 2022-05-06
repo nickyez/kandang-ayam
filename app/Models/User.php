c593be8cb44ebaf29621103d516e2f3e0635b13f
NickyErlanggaZ <nickyerlanggaz@gmail.com>
2022-05-06 13:25:25 +0700

    add: model for user

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,8 +19,11 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'username',
         'email',
         'password',
+        'level',
+        'photos_url',
     ];
 
     /**
