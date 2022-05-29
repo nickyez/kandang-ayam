d9fa934437901795d5f384749b2c0ad9c2a158ca
NickyErlanggaZ <nickyerlanggaz@gmail.com>
2022-05-29 21:18:22 +0700

    update: using User model instead DB

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class UserSeeder extends Seeder
 {
@@ -15,13 +15,26 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => 'Admin',
-            'username'=> 'admin',
-            'email' => 'admin'.'@gmail.com',
-            'password' => Hash::make('admin'),
-            'level'=> "admin",
-            'photos_url'=> "undraw_profile.svg"
-        ]);
+        User::truncate();
+        User::insert(
+            [
+                [
+                    'name' => 'Admin',
+                    'username'=> 'admin',
+                    'email' => 'admin'.'@gmail.com',
+                    'password' => Hash::make('admin'),
+                    'is_admin'=> 1,
+                    'photos_url'=> "undraw_profile.svg"
+                ],
+                [
+                    'name' => 'User',
+                    'username'=> 'user',
+                    'email' => 'user'.'@gmail.com',
+                    'password' => Hash::make('user'),
+                    'is_admin'=> 0,
+                    'photos_url'=> "undraw_profile.svg"
+                ]
+            ]
+        );
     }
 }
