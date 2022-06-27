e29b3eaf84e9ff073168b5593c854361302d9077
NickyErlanggaZ <nickyerlanggaz@gmail.com>
2022-06-27 21:23:29 +0700

    update: form can update data

--- resources/views/pages/controls/lampu.blade.php
+++ resources/views/pages/controls/lampu.blade.php
@@ -99,62 +99,107 @@
             <div class="card-header py-3">
                 <h6 class="m-0 font-weight-bold text-primary">Status Lampu</h6>
             </div>
-            <form action="#" method="post">
+            <form action="/update-lampu" method="post">
+                @csrf
+                @method('PUT')
                 <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success alert-dismissible fade show" role="alert">
+                            {{ session('status') }}
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                    @endif
                     <div class="form-group">
                         <label for="mode" class="small mb-1">Mode</label>
                         <div id="mode" class="btn-group btn-group-toggle btn-group-sm mx-4" data-toggle="buttons">
-                            <label class="btn btn-outline-primary active">
-                                <input type="radio" name="mode" id="otomatis" @if($getLastLampStatus->mode == '0') selected @endif value="0" autocomplete="off"
-                                    checked>
+                            <label class="btn btn-outline-primary  @if ($getLastLampStatus->mode == 0) active @endif">
+                                <input type="radio" name="mode" id="otomatis" value="0"
+                                    @if ($getLastLampStatus->mode == 0) checked @endif
+                                    autocomplete="off">
                                 Otomatis
                             </label>
-                            <label class="btn btn-outline-primary">
-                                <input type="radio" name="mode" id="manual" @if($getLastLampStatus->mode == '1') selected @endif  value="1" autocomplete="off">
+                            <label class="btn btn-outline-primary @if ($getLastLampStatus->mode == 1) active @endif">
+                                <input type="radio" name="mode" id="manual" value="1"
+                                    @if ($getLastLampStatus->mode == 1) checked @endif
+                                    autocomplete="off">
                                 Manual
                             </label>
                         </div>
                     </div>
-                    <div class="row" id='status'>
-                        <div class="col-md-4">
-                            <div class="small mb-1">Batas suhu untuk lampu mati saat ini</div>
-                            <div class="medium mb-1 text-danger"><strong>{{$getLastLampStatus->suhu_mati ?? 'N/A'}}</strong></div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="small mb-1">Batas suhu untuk lampu menyala saat ini</div>
-                            <div class="medium mb-1 text-success"><strong>{{$getLastLampStatus->suhu_nyala ?? 'N/A'}}</strong></div>
-                        </div>
-                    </div>
-                    <div class="row" id="show">
-                        <div class="form-group col-md-4">
-                            <div class="small mb-1">Batas suhu untuk lampu mati</div>
-                            <div class="row justify-content-center align-items-center">
-                                <div class="col-9">
-                                    <input type="range" class="custom-range w-100" min="25" max="35"
-                                        value="30" name="suhu" id="suhu-mati"
-                                        oninput="document.getElementById('label-suhu-mati').innerHTML = this.value">
+                    @if ($getLastLampStatus->mode == 0)
+                        <div class="row" id='status'>
+                            <div class="col-md-4">
+                                <div class="small mb-1">Batas suhu untuk lampu mati saat ini</div>
+                                <div class="medium mb-1 text-danger">
+                                    <strong>{{ $getLastLampStatus->suhu_mati ?? 'N/A' }}</strong>
                                 </div>
-                                <div class="col-3">
-                                    <span class="small mb-1 text-center" id="label-suhu-mati">30</span>
-                                    <span class="small mb-1">°C</span>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="small mb-1">Batas suhu untuk lampu menyala saat ini</div>
+                                <div class="medium mb-1 text-success">
+                                    <strong>{{ $getLastLampStatus->suhu_nyala ?? 'N/A' }}</strong>
                                 </div>
                             </div>
                         </div>
-                        <div class="form-group col-md-4">
-                            <div class="small mb-1">Batas suhu untuk lampu menyala</div>
-                            <div class="row justify-content-center align-items-center">
-                                <div class="col-9">
-                                    <input type="range" class="custom-range w-100" min="25" max="35"
-                                        value="30" name="suhu" id="suhu-nyala"
-                                        oninput="document.getElementById('label-suhu-nyala').innerHTML = this.value">
+                        <div class="row" id="show">
+                            <div class="form-group col-md-4">
+                                <div class="small mb-1">Batas suhu untuk lampu mati</div>
+                                <div class="row justify-content-center align-items-center">
+                                    <div class="col-9">
+                                        <input type="range" class="custom-range w-100" min="25" max="35"
+                                            value="{{ $getLastLampStatus->suhu_mati }}" name="suhu_mati" id="suhu_mati"
+                                            oninput="document.getElementById('label-suhu-mati').innerHTML = this.value">
+                                    </div>
+                                    <div class="col-3">
+                                        <span class="small mb-1 text-center"
+                                            id="label-suhu-mati">{{ $getLastLampStatus->suhu_mati }}</span>
+                                        <span class="small mb-1">°C</span>
+                                    </div>
                                 </div>
-                                <div class="col-3">
-                                    <span class="small mb-1 text-center" id="label-suhu-nyala">30</span>
-                                    <span class="small mb-1">°C</span>
+                            </div>
+                            <div class="form-group col-md-4">
+                                <div class="small mb-1">Batas suhu untuk lampu menyala</div>
+                                <div class="row justify-content-center align-items-center">
+                                    <div class="col-9">
+                                        <input type="range" class="custom-range w-100" min="25" max="35"
+                                            value="{{ $getLastLampStatus->suhu_nyala }}" name="suhu_nyala"
+                                            id="suhu_nyala"
+                                            oninput="document.getElementById('label-suhu-nyala').innerHTML = this.value">
+                                    </div>
+                                    <div class="col-3">
+                                        <span class="small mb-1 text-center"
+                                            id="label-suhu-nyala">{{ $getLastLampStatus->suhu_nyala }}</span>
+                                        <span class="small mb-1">°C</span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    @else
+                        <div class="row" id="status">
+                            <div class="col-md-4">
+                                <div class="small mb-1">Batas jam untuk lampu mati saat ini</div>
+                                <div class="medium mb-1 text-danger">
+                                    <strong>{{ $getLastLampStatus->time_off ?? 'N/A' }}</strong></div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="small mb-1">Batas jam untuk lampu menyala saat ini</div>
+                                <div class="medium mb-1 text-success">
+                                    <strong>{{ $getLastLampStatus->time_on ?? 'N/A' }}</strong></div>
+                            </div>
+                        </div>
+                        <div class="row" id="show">
+                            <div class="form-group col-md-4">
+                                <div class="small mb-1">Jam Mulai lampu menyala</div>
+                                <input type="time" class="form-control" name="time_on" id="time_on">
+                            </div>
+                            <div class="form-group col-md-4">
+                                <div class="small mb-1">Jam Akhir lampu mati</div>
+                                <input type="time" class="form-control" name="time_off" id="time_off">
+                            </div>
+                        </div>
+                    @endif
                     <div class="card-footer">
                         <div class="col-sm-12 mb-3">
                             <button type="submit" class="btn btn-primary float-right btn-sm"><i class="fas fa-edit"></i>
@@ -200,11 +245,11 @@
                         $('#status').html(`
                         <div class="col-md-4">
                             <div class="small mb-1">Batas jam untuk lampu mati saat ini</div>
-                            <div class="medium mb-1 text-danger"><strong>23</strong></div>
+                            <div class="medium mb-1 text-danger"><strong>{{ $getLastLampStatus->time_off ?? 'N/A' }}</strong></div>
                         </div>
                         <div class="col-md-4">
                             <div class="small mb-1">Batas jam untuk lampu menyala saat ini</div>
-                            <div class="medium mb-1 text-success"><strong>23</strong></div>
+                            <div class="medium mb-1 text-success"><strong>{{ $getLastLampStatus->time_on ?? 'N/A' }}</strong></div>
                         </div>
                         `)
                         $('#show').html(`
@@ -222,11 +267,11 @@
                         $('#status').html(`
                         <div class="col-md-4">
                         <div class="small mb-1">Batas suhu untuk mati saat ini</div>
-                        <div class="medium mb-1 text-danger"><strong>{{$getLastLampStatus->suhu_mati ?? 'N/A'}}</strong></div>
+                        <div class="medium mb-1 text-danger"><strong>{{ $getLastLampStatus->suhu_mati ?? 'N/A' }}</strong></div>
                     </div>
                     <div class="col-md-4">
                         <div class="small mb-1">Batas suhu nyala saat ini</div>
-                        <div class="medium mb-1 text-success"><strong>{{$getLastLampStatus->suhu_nyala ?? 'N/A'}}</strong></div>
+                        <div class="medium mb-1 text-success"><strong>{{ $getLastLampStatus->suhu_nyala ?? 'N/A' }}</strong></div>
                     </div>
                         `)
                         $('#show').html(`
@@ -235,11 +280,11 @@
                         <div class="row justify-content-center align-items-center">
                             <div class="col-9">
                                 <input type="range" class="custom-range w-100" min="25" max="35"
-                                    value="30" name="suhu" id="suhu-mati"
+                                    value="{{ $getLastLampStatus->suhu_mati }}" name="suhu_mati" id="suhu_mati"
                                     oninput="document.getElementById('label-suhu-mati').innerHTML = this.value">
                             </div>
                             <div class="col-3">
-                                <span class="small mb-1 text-center" id="label-suhu-mati">30</span>
+                                <span class="small mb-1 text-center" id="label-suhu-mati">{{ $getLastLampStatus->suhu_mati }}</span>
                                 <span class="small mb-1">°C</span>
                             </div>
                         </div>
@@ -249,11 +294,11 @@
                         <div class="row justify-content-center align-items-center">
                             <div class="col-9">
                                 <input type="range" class="custom-range w-100" min="25" max="35"
-                                    value="30" name="suhu" id="suhu-nyala"
+                                    value="{{ $getLastLampStatus->suhu_nyala }}" name="suhu_nyala" id="suhu_nyala"
                                     oninput="document.getElementById('label-suhu-nyala').innerHTML = this.value">
                             </div>
                             <div class="col-3">
-                                <span class="small mb-1 text-center" id="label-suhu-nyala">30</span>
+                                <span class="small mb-1 text-center" id="label-suhu-nyala">{{ $getLastLampStatus->suhu_nyala }}</span>
                                 <span class="small mb-1">°C</span>
                             </div>
                         </div>
