199bc4221c29a6f653b67f5858fd0e43f8c61c1f
NickyErlanggaZ <nickyerlanggaz@gmail.com>
2022-06-27 21:24:23 +0700

    add: parameter d in dashboard and lamp control

--- resources/views/template/components/sidebar.blade.php
+++ resources/views/template/components/sidebar.blade.php
@@ -1,7 +1,7 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{url('/')}}">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ url('/') }}">
         <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-egg"></i>
         </div>
@@ -10,10 +10,10 @@
 
     <!-- Divider -->
     <hr class="sidebar-divider my-0">
-    
+
     <!-- Nav Item - Dashboard -->
-    <li class="nav-item @if(Request::is('/')) active @endif">
-        <a class="nav-link" href="{{url('/?d='.session()->get('device'))}}">
+    <li class="nav-item @if (Request::is('/')) active @endif">
+        <a class="nav-link" href="{{ url('/?d=' . session()->get('device')) }}">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span></a>
     </li>
@@ -27,34 +27,49 @@
     </div>
 
     <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item @if(Request::url() == url('/kontrol-lampu')) active @endif">
-        <a class="nav-link" href="{{url('/kontrol-lampu?d='.session()->get('device'))}}">
+    <li class="nav-item @if (Request::url() == url('/kontrol-lampu')) active @endif">
+        <a class="nav-link" href="{{ url('/kontrol-lampu?d=' . session()->get('device')) }}">
             <i class="fas fa-fw fa-cog"></i>
             <span>Lampu</span>
         </a>
     </li>
-    @if(Auth::user()->is_admin == 1)
-    <!-- Divider -->
-    <hr class="sidebar-divider">
+    @if (Auth::user()->is_admin == 1)
+        <!-- Divider -->
+        <hr class="sidebar-divider">
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Super Admin Menu
-    </div>
+        <!-- Heading -->
+        <div class="sidebar-heading">
+            Super Admin Menu
+        </div>
 
-    <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item @if(Request::url() == url('/users')) active @endif">
-        <a class="nav-link" href="{{url('/users')}}">
-            <i class="fas fa-fw fa-chart-area"></i>
-            <span>Users</span></a>
-    </li>
+        <!-- Nav Item - Pages Collapse Menu -->
+        <li class="nav-item @if (Request::url() == url('/users')) active @endif">
+            <a class="nav-link" href="{{ url('/users') }}">
+                <i class="fas fa-fw fa-chart-area"></i>
+                <span>Users</span></a>
+        </li>
 
-    <!-- Nav Item - Charts -->
-    <li class="nav-item @if(Request::url() == url('/devices')) active @endif">
-        <a class="nav-link" href="{{url('/devices')}}">
-            <i class="fas fa-fw fa-chart-area"></i>
-            <span>Devices</span></a>
-    </li>
+        <!-- Nav Item - Charts -->
+        <li class="nav-item @if (Request::url() == url('/devices')) active @endif">
+            <a class="nav-link" href="{{ url('/devices') }}">
+                <i class="fas fa-fw fa-chart-area"></i>
+                <span>Devices</span></a>
+        </li>
+    @else
+        <!-- Divider -->
+        <hr class="sidebar-divider">
+
+        <!-- Heading -->
+        <div class="sidebar-heading">
+            Manajemen Device
+        </div>
+
+        <!-- Nav Item - Charts -->
+        <li class="nav-item @if (Request::url() == url('/device')) active @endif">
+            <a class="nav-link" href="{{ url('/device') }}">
+                <i class="fas fa-fw fa-chart-area"></i>
+                <span>Devices</span></a>
+        </li>
     @endif
     <!-- Divider -->
     <hr class="sidebar-divider d-none d-md-block">
